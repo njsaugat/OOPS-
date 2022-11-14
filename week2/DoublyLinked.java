022d5fb90a21506ed0765d16495f8ba3820ea2d3
Saugat Poudel <njsaugat@gmail.com>
2022-11-14 07:59:33 +0545

    doubly linked list added

--- week2/DoublyLinked.java
+++ week2/DoublyLinked.java
@@ -11,21 +11,78 @@ public class DoublyLinked {
             }
             Node newNode=new Node();
             newNode.data=data;
+            newNode.next=null;
+            temp.next=newNode;
+            newNode.prev=temp;            
+        }
+    }
+
+    public static Node insertAtFirstDoubly(Node head, int data){
+        Node newNode=new Node();
+        newNode.data=data;
+
+        newNode.next=head;
+        newNode.prev=null;
+        
+        head.prev=newNode;
+
+        head=newNode;
+        return head;
+        
+    }
+
+
+    public static void  insertInSpecificPosition(Node head, int position, int data){
+        if(head.data==0 && head.next==null){
+            head.data=data;
+        }else{
+            Node temp=head;
+
+            for(int i=1;i<position;i++){
+                temp=temp.next;
+            }
+            Node nextNode=temp.next;
+            Node newNode=new Node();
+            newNode.data=data;
             temp.next=newNode;
             newNode.prev=temp;
+
+            newNode.next=nextNode;
+            nextNode.prev=newNode;
+
         }
+    }
+
+
+    public static void remove(Node head, int position){
+        Node temp=head;
+        for(int i=1;i<position-1;i++){
+            temp=temp.next;
+        }
+
+        Node nextNode=temp.next;
+
+        if(nextNode.next==null){//at last
+            temp.next=null;
+            
+        }
+
+        temp.next=nextNode.next;
+        nextNode.prev=temp;
     }
 
     public static void display(Node head){
         Node temp=head;
         while(temp!=null){
             System.out.println(temp.data);
+            temp=temp.next;
         }
     }
 
     public static void main(String args[]){
         Node head=new Node();
-        
+        head.prev=null;
+        head.next=null;
         insertInDoubly(head, 5);
         insertInDoubly(head, 6);
         insertInDoubly(head, 7);
