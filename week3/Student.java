3c5b73adbca68a9f8f5cdb2e0142e0983b94da24
Saugat Poudel <njsaugat@gmail.com>
2022-11-25 12:17:07 +0545

    student file added

---
+++ week3/Student.java
@@ -0,0 +1,17 @@
+package week3;
+
+public class Student{
+    public String name;
+    public int roll_no;
+    
+    public Student(String name, int roll_no){
+        this.name=name;
+        this.roll_no=roll_no;
+    }
+
+    public static void main(String[] args){
+        Student John=new Student("John",2);
+        
+
+    }
+}
