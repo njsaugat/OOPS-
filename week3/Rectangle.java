3c877fb0faaeef03bb991319e84292e6d338616d
Saugat Poudel <njsaugat@gmail.com>
2022-12-04 07:48:33 +0545

    rectangle file added

--- week3/Rectangle.java
+++ week3/Rectangle.java
@@ -17,21 +17,15 @@ public class Rectangle {
         return length*breadth;
     }
     public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);  
         System.out.println("Enter the length of rectangle ");
         double length=sc.nextDouble();
         System.out.println("Enter the breadth of rectangle ");
         double breadth=sc.nextDouble();
         Rectangle r1=new Rectangle(length, breadth);
-        System.out.println(r1.findArea());
+        System.out.println("The area is "+ r1.findArea());
 
-
-        // Rectangle r1=new Rectangle(4, 5);
-        // Rectangle r2=new Rectangle(5, 8);
-        // System.out.println(r1.perimeter());
-        // System.out.println(r1.findArea());
-        // System.out.println(r2.perimeter());
-        // System.out.println(r2.findArea());
-
+       
     }
 }
