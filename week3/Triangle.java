17224c9cbf3dfcdb7801b7cef07c4cbbe09f6b9c
Saugat Poudel <njsaugat@gmail.com>
2022-12-04 07:48:46 +0545

    triangle file added

--- week3/Triangle.java
+++ week3/Triangle.java
@@ -9,7 +9,6 @@ public class Triangle {
         return this.firstSide+this.secondSide+this.thirdSide;
     }
     public double findArea(){
-        // double perimeter=perimeter();
         double s=perimeter()/2;
         double interMediate=s*(s-this.firstSide)*(s-this.secondSide)*(s-this.thirdSide);
         return Math.sqrt(interMediate);
@@ -21,7 +20,7 @@ public class Triangle {
         t1.firstSide=3;
         t1.secondSide=4;
         t1.thirdSide=5;
-        System.out.println(t1.findArea());
+        System.out.println("The area is "+t1.findArea());
 
     }
 
