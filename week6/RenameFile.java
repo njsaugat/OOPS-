26fbd122ab9fb6863f174a7e12f3b050c8c94447
Saugat Poudel <njsaugat@gmail.com>
2023-01-20 09:34:41 +0545

    rename file added

---
+++ week6/RenameFile.java
@@ -0,0 +1,21 @@
+package week6;
+
+import java.io.File;
+
+public class RenameFile {
+    public static void main(String[] args) {
+        // current file path
+        File oldFile = new File("/abc.txt");
+        // new file path
+        File newFile = new File("/xyz.txt");
+
+        // rename file
+        boolean success = oldFile.renameTo(newFile);
+
+        if (success) {
+            System.out.println("File renamed successfully");
+        } else {
+            System.out.println("File rename failed");
+        }
+    }
+}
